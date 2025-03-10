6f665607951975c9388ff102434a11113c4106ef
DongCuong <geas0621@gmail.com>
2025-03-10 16:57:50 +0200

    Added: PUG templating engine

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -7,6 +7,9 @@ const userRoutes = require('./routes/shop')
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(userRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(3000);
--- ExpressJs/routes/admin.js
+++ ExpressJs/routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-products', { PageTitle: "Add Products" })
 });
 
 router.post('/add-product', (req, res, next) => {
