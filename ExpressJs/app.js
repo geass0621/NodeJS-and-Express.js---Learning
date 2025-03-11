c1a99cab47b6a22bc398909ebe20f6bfe3775e69
DongCuong <geas0621@gmail.com>
2025-03-11 19:58:25 +0200

    Added: ejs and removed pug, handlebars

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
+
 
 const adminData = require('./routes/admin');
 const userRoutes = require('./routes/shop')
 
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
