3b1884008a1d41412d8107e637d27fd5605f1240
DongCuong <geas0621@gmail.com>
2025-03-12 21:26:20 +0200

    Added: mvc pattern

--- ExpressJs/routes/admin.js
+++ ExpressJs/routes/admin.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const path = require('path')
 
-const rootDir = require('../utils/path');
-const { title } = require('process');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: "Add Products", path: "/admin/add-product", activeAddProduct: true, formsCSS: true, productCSS: true })
-});
+router.post('/add-product', productController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title })
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- ExpressJs/routes/shop.js
+++ ExpressJs/routes/shop.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const path = require('path');
 
-const adminData = require('./admin')
-
-const rootDir = require('../utils/path')
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
-});
+router.get('/', productController.getProducts);
 
 module.exports = router;
