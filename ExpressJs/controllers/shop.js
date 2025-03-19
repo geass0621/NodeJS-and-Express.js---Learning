d5328996c0917c67d6fab572f916719a5c85a2e2
DongCuong <geas0621@gmail.com>
2025-03-19 17:36:38 +0200

    Update: delete and edit product func

--- ExpressJs/controllers/shop.js
+++ ExpressJs/controllers/shop.js
@@ -2,24 +2,24 @@ const Product = require('../models/product');
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products',
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      res.render('shop/product-detail', { product: product[0], pageTitle: product[0].title, path: '/products' })
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 
 }
 
@@ -60,17 +60,15 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/',
       })
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err))
 };
 
 exports.getCheckout = (req, res, next) => {
