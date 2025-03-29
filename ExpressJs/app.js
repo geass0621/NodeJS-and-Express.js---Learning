96b5495192f1e3589fe5352a04059bc171dffddb
DongCuong <geas0621@gmail.com>
2025-03-29 17:15:24 +0200

    Added: sessions

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -7,18 +7,32 @@ const mongoose = require('mongoose');
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://dongcuong0621:NdLTGXBI5TpinOVK@cluster0.qhvix.mongodb.net/Shop?retryWrites=true&w=majority&appName=Cluster0';
+
 
 const app = express();
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collection: 'session'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
 
 app.use((req, res, next) => {
   User.findById('67e56bdc22db2a938a01c491')
