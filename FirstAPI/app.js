9a9394de9b0d424a73e62a83290066728ed5ca8c
DongCuong <geas0621@gmail.com>
2025-04-11 17:20:30 +0300

    Set: server to serve images static and improve error handling with middleware

--- FirstAPI/app.js
+++ FirstAPI/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const feedRoutes = require('./routes/feed');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json()); //application/json
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,6 +17,17 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+
+  res.status(status)
+    .json({
+      message: message
+    });
+})
 
 mongoose
   .connect('mongodb+srv://dongcuong0621:NdLTGXBI5TpinOVK@cluster0.qhvix.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
