9d30960cf45c52bc9798ffef2b1b9f6e90bc2256
DongCuong <geas0621@gmail.com>
2025-04-14 18:02:06 +0300

    Update: validation with jwt token

--- FirstAPI/routes/auth.js
+++ FirstAPI/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body } = require('express-validator');
 const User = require('../models/user');
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/isAuth');
 
 router.put('/signup',
   [
--- FirstAPI/routes/feed.js
+++ FirstAPI/routes/feed.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { body } = require('express-validator');
+const isAuth = require('../middleware/isAuth');
 
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 router.post('/post',
   [
